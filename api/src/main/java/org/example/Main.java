1ef9d5e5591a7c437ffe91137975b4985baf3c68
Michal Parszyk <parszyk.m@wp.pl>
2025-01-02 20:57:01 +0100

    test

--- api/src/main/java/org/example/Main.java
+++ api/src/main/java/org/example/Main.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TheaterApplication {
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(TheaterApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }

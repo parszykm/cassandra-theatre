094317f87245479dfcca07aaf8b4d7b62540fd65
Michal Parszyk <parszyk.m@wp.pl>
2025-01-04 18:24:13 +0100

    Python migration

---
+++ python/README.md
@@ -0,0 +1,5 @@
+# Environment
+The application must be run on environment with Python installed at minimal version of 3.11
+```
+python3.11 main.py
+```
--- python/main.py
+++ python/main.py
@@ -9,36 +9,33 @@ class BackendSession:
         config.read('cluster.config')
         self.contact_point = config['ClusterConfig']['contact_point']
         self.keyspace = config['ClusterConfig']['keyspace']
-        # print(f"Contact Point: {contact_point}")
-        # print(f"Keyspace: {keyspace}")
         try:
             self.cluster = Cluster([self.contact_point])
-            self.session = self.cluster.connect(self.keyspace)   
+            self.session = self.cluster.connect(self.keyspace)
         except Exception as e:
             print(f"Couldn't connect to cluster:{e}", file=sys.stderr)
     def get_all_shows(self):
-        query = "SELECT * title FROM shows"
+        query = "SELECT * FROM shows"
         rows = self.session.execute(query)
-        return [{"show_id": str(row.show_id), 
-                 "show_date": str(row.show_date), 
-                 "show_time": str(row.show_time), 
+        return [{"show_id": str(row.show_id),
+                 "show_date": str(row.show_date),
+                 "show_time": str(row.show_time),
                  "title": row.title} for row in rows]
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
-@app.route('/', methods = ['GET', 'POST']) 
-def home(): 
-	if(request.method == 'GET'): 
-		data = "hello world"
-		return jsonify({'data': data}) 
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+	if(request.method == 'GET'):
+		return jsonify({'description': 'Cassandra Theatre API'})
 
 
-@app.route('/shows', methods = ['GET']) 
+@app.route('/shows', methods = ['GET'])
 def get_shows():
 	backend = BackendSession()
-	shows = backend.get_shows()
-	return jsonify({'shows': shows}) 
+	shows = backend.get_all_shows()
+	return jsonify({'shows': shows})
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
 
-	app.run(debug = True) 
+	app.run(debug = True,host='0.0.0.0', port=8080)

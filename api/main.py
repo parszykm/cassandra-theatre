97d4ce0de78769a227803f2d388e69581352c3d6
Michal Parszyk <parszyk.m@wp.pl>
2025-01-05 16:51:15 +0100

    New endpoints

--- api/main.py
+++ api/main.py
@@ -4,6 +4,13 @@ import configparser
 import sys
 import uuid
 from datetime import datetime
+import string
+from enum import Enum
+
+class Status(Enum):
+    AVAILABLE = 'available'
+    RESERVED = 'reserved'
+    SOLD = 'sold'
 
 class BackendSession:
     def __init__(self):
@@ -36,27 +43,108 @@ class BackendSession:
             INSERT INTO shows (show_id, show_date, show_time, title)
             VALUES (%s, %s, %s, %s)
             """
-            show_id = uuid.uuid4()  # Generate a unique ID for the show
+            show_id = uuid.uuid4()
             self.session.execute(query, (show_id, show_date, show_time, title))
-            self.populate_seats_for_show()
+            self.populate_seats_for_show(show_id)
             
             return str(show_id)
         except Exception as e:
             print(f"Error adding show: {e}", file=sys.stderr)
             return None
-    def populate_seats_for_show(self, show_id, total_seats=20):
+    def populate_seats_for_show(self, show_id, rows=5, total_seats=10):
             try:
                 query = """
                 INSERT INTO seats_by_show (show_id, seat_id, seat_number, status)
                 VALUES (%s, %s, %s, %s)
                 """
-                for seat_number in range(1, total_seats + 1):
-                    seat_id = f"seat-{seat_number}"
-                    status = "available"
-                    self.session.execute(query, (show_id, seat_id, seat_number, status))
+                for row in range(rows):
+                    for seat_number in range(1, total_seats + 1):
+                        seat_id = f"{string.ascii_uppercase[row]}-{seat_number}"
+                        status = "available"
+                        self.session.execute(query, (show_id, seat_id, seat_number, status))
             except Exception as e:
                 print(f"Error populating seats for show {show_id}: {e}", file=sys.stderr)
-
+                return None
+    def get_seats(self, show_id, status=Status.AVAILABLE.value):
+        try:
+            query="""
+            SELECT seat_id FROM seats_by_show
+            WHERE show_id = %s AND status = %s
+            """
+            rows = self.session.query(query, (show_id, status))
+            return [str(row.seat_id) for row in rows]
+        except Exception as e:
+            print(f"Could not get the seats: {e}", file=sys.stderr)
+            return None
+        
+    def get_reservations(self, email):
+        try:
+            query = """
+            SELECT show_id, tickets_count, reservation_id, reservation_time FROM reservations_info
+            WHERE email=%s
+            """
+            result = []
+            rows = self.session.execute(query, (email))
+            for row in rows:
+                reservation_id = str(row.reservation_id)
+                show_id = str(row.show_id)
+                reservation = {
+                    "email": email,
+                    "tickets": str(row.tickets_count),
+                    "reservation_time": str(row.reservation_time)
+                    }
+                
+                query = """
+                SELECT title FROM shows
+                WHERE show_id=%s
+                """
+                show = self.session.execute(query, (show_id))
+                reservation["title"] = str(show[0].title)
+                
+                query = """
+                SELECT seat_id FROM reservations_by_user
+                WHERE reservation_id=%s
+                """
+                
+                seats_rows = self.session.execute(query, (reservation_id))
+                seats = []
+                for seat in seats_rows:
+                    seats.append(str(seat.seat_id))
+                reservation["seats"] = seats
+                result.append(reservation)
+            return result
+        except Exception as e:
+            print(f"Error while getting reservation: {e}")
+            return None
+    def check_seats_availability(self, show_id, seats):
+        available_seats = set(self.get_seats(show_id))
+        return all(seat in available_seats for seat in seats)
+    def make_reservation(self, email, show_id, seats, user_name):
+        try:
+            query="""
+            INSERT INTO reservations_by_user (reservation_id, show_id, seat_reservation_time, seat_id)
+            VALUES (%s, %s, %s, %s)
+            """
+            reservation_id = uuid.uuid4()
+            reservation_time = datetime.now()
+            
+            ### Check if chosen seats are not already reserved
+            if self.check_seats_availability(show_id, seats):
+                return 1
+            for seat in seats:
+                self.session.execute(query, (reservation_id, show_id, reservation_time, seat))
+            
+            query="""
+            INSERT INTO reservation_info (reservation_id, show_id, tickets_count, user_name, email, reservation_time)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """
+            ticket_count = len(seats)
+            self.session.execute(query, (reservation_id, show_id, ticket_count, user_name, email, reservation_time))
+            
+        except Exception as e:
+            print(f"Error while creating reservation: {e}")
+            return 2
+        
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
@@ -66,7 +154,7 @@ def home():
 
 
 @app.route('/shows', methods = ['GET', 'POST'])
-def get_shows():
+def shows():
     backend = BackendSession()
     if request.method == 'GET':
         shows = backend.get_all_shows()
@@ -85,11 +173,46 @@ def get_shows():
                 if show_id is not None:
                     return jsonify({'message': 'Show added successfully', 'show_id': show_id}), 201
                 else:
-                    return jsonify({'error': f'Could not add show'}), 500
+                    return jsonify({'error': f'Could not add the show'}), 500
             except Exception as e:
-                return jsonify({'error': f'Could not add show: {e}'}), 500
+                return jsonify({'error': f'Could not add the show: {e}'}), 500
         
+@app.route('/shows', methods=['GET','POST'])
+def reservations():
+    backend = BackendSession()
+    data = request.json
+    if request.method == 'GET':
+        email = data['email']
+        reservations = backend.get_reservations(email=email)
+        if reservations is not None:
+            return jsonify(reservations), 201
+        else:
+            return jsonify({'error': f'Could not get the reservation'}), 500
+    elif request.method == 'POST':
+        email = data['email']
+        user_name = data['user_name']
+        show_id = data['show_id']
+        seats = data['seats']
+        rc = backend.make_reservation(email, show_id, seats, user_name)
+        if rc == 0:
+            return jsonify({'message': 'The reservation has been created'}), 201
+        elif rc == 1:
+            return jsonify({'error': 'At least one of the chosen seats has been already reserved'}), 409
+        elif rc == 2:
+            return jsonify({'error': 'Error while making reservation'}), 500
+        
+@app.route('/seats', methods=['GET'])
+def seats():
+    backend = BackendSession()
+    data = request.json
+    if request.method == 'GET':
+        show_id = data['show_id']
+        seats = backend.get_seats(show_id)
+        if seats is not None:
+            return jsonify(seats), 201
+        else:
+            return jsonify({'error': f'Could not get the seats'}), 500
 
+        
 if __name__ == '__main__':
-
 	app.run(debug = True,host='0.0.0.0', port=8080)
